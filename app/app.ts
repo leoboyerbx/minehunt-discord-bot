e1866183d171a4ddabbb6fa491d7855886f41f77
leoboyerbx <contact@leoboyer.fr>
2020-05-19 18:48:10 +0200

    update gitignore

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,8 @@
 import { Minehut } from "./lib/minehut-api";
+import dotenv from 'dotenv'
 
 (async () => {
+    console.log(process.env.EMAIL)
     const minehut = new Minehut()
     await minehut.login('lboyer1@live.fr', 'Pee@gu0311')
     const myserver = await minehut.servers.fetchOneByName('leoetzozo')
